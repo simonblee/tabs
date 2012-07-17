0bbc81dc877c3d27501891f15211fdfbfe3fa195
simon <simblee@gmail.com>
2012-07-17 16:43:16 +0800

    Changed plugin to use closures for storage of state. In the process reduced the plugin size by about half.

--- tabs.js
+++ tabs.js
@@ -1,101 +1,43 @@
-
 (function($){
+	$.fn.tabs = function ( options ) {
 
+		return this.each( function () {  
+			var menu,
+				mkp = '',
+				$this = $(this),
+				tmpl = '<li class="{class}"><a href="#{id}"><span>{title}</span></a></li>',
+				elements = $this.find('.tab-container');
 
-	var Tabs = function(){
-		this.options = {
-			menu: null,
-			onShow: function(){}
-		}
-	}
-
-	Tabs.prototype = {
-
-        constructor: Tabs,
-
-        init: function( o ){
-        	$.extend( this.options, o );
-
-			//Create the tab menu markup
-			this._createMarkup();
-
-			//Add the events to the tabs
-			this._bindEvents();
-			this._showAndHide( this.options.menu.find('.lefttab') );
-        },
-
-		_bindEvents: function(){
-			var self = this;
-			this.options.menu.find('li').each(function(){
-                //Bind the click event to display/hide the tabs
-                $(this).bind( 'click.tabs', function( event ){
-                	event.preventDefault();
-                    self._showAndHide( $(this) );
-                });
-            });
-		},
-
-		_showAndHide: function( tab ){
-			var id = tab.find('a').attr('href');
-
-			//Show the tab
-            tab.addClass('tabactive').siblings().removeClass('tabactive');   
-
-            //Display the correct tab container
-            this.element.find('#'+id).show()
-            			.siblings('.tab-container').hide();
-            
-            //Trigger the custom onShow event
-            this.options.onShow( this.element.find('#'+id) );
-		},
-
-		_createMarkup: function(){
-			var i,
-				mkp = '<div class="tabs-menu"><ul>',
-				elements = this.element.find('.tab-container');
-			
-			//Create menu markup
-			for(i=0;i<elements.length;i++){
-				elem = $(elements[i]);
-				mkp += 	'<li>'+
-							'<a href="#'+elem.attr('id')+'">'+
-								'<span>'+elem.attr('title')+'</span>'+
-							'</a>'+
-						'</li>';
+			//Return if no tab containers found
+			if( !$this.find('.tab-container').length ){
+				return;
 			}
 
-			//Add the menu markup
-			if( this.options.menu ){
-				this.options.menu.prepend( '<ul>' + mkp + '</ul>' )
-								 .addClass('tabs-menu');
-			} else {
-				this.element.prepend( '<div class="tabs-menu"><ul>' + mkp + '</ul></div>' );
-				this.options.menu = this.element.find('.tabs-menu');
-			}
+			// Overwrite defaults
+			defaults = {
+				menu: null,
+				show: 0, //index of tab to show on init
+				onShow: function(){}
+			};
+			options = $.extend( {}, defaults, options );
 
-			//Assign left and right tabs, show left
-			this.options.menu.find('li:first').addClass('lefttab');
-			this.options.menu.find('li:last').addClass('righttab');			
-		}
+			// Create markup
+			for( i=0; i<elements.length; i++ ){
+				cls = i === 0 ? 'lefttab' : ( i === elements.length - 1  ? 'righttab' : '' );
+				mkp +=  tmpl.replace( '{class}', cls )
+							.replace( '{id}', $(elements[i]).attr('id') )
+							.replace( '{title}', $(elements[i]).attr('title') );
+			}
+			mkp = '<ul>' + mkp + '</ul>';
+			menu = options.menu ? $( mkp ).prependTo( options.menu ) : $( '<div class="tabs-menu">'+mkp+'</div>' ).prependTo( $this );
+
+			// Bind the keyup event, and show the desired tab
+			menu.find('li').on( 'click.tabs', function ( event ) {
+				$(this).addClass('tabactive').siblings().removeClass('tabactive');   
+				$this.find( '#' + $(this).find('a').attr('href') ).show().siblings('.tab-container').hide();
+				options.onShow.call( $(this) );
+				event.preventDefault();
+			}).eq( options.show ).trigger( 'click.tabs' );
+		});
 	};
-
-	$.fn.tabs = function( method ){
-		var data, args = arguments;
-
-		//Return if no tab containers found
-		if( !this.find('.tab-container').length ){
-			return;
-		}
-	
-		return this.each(function(){     
-            data = $(this).data('tabs');            
-            if( !data ){
-            	//Instantiate tabs
-                $(this).data('tabs', { o : new Tabs() });
-                data = $(this).data('tabs');
-                data.o.element = $(this);
-                data.o.init( method ); //'method' will be options object here
-            }     
-        });  
-	}
 })(jQuery);
